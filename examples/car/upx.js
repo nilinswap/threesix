41edcd05093e3a420f365da02c7d88e4544b15fe
Swapnil Sharma <nilinswap@gmail.com>
2021-04-08 00:23:17 +0530

    should work

---
+++ examples/car/upx.js
@@ -0,0 +1,132 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
+
+
+const scene = new THREE.Scene();
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight);
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const camForm = document.getElementById('control-form')
+const camdist = document.getElementById('camdist')
+const camupx = document.getElementById('camupx-button');
+const camupz = document.getElementById('camupz-button');
+const camupy = document.getElementById('camupy-button');
+camdist.max = cameraWidth;
+camdist.value = cameraWidth / 2;
+
+
+var camera = new THREE.OrthographicCamera(
+    cameraWidth / -2, // left
+    cameraWidth / 2, // right
+    cameraHeight / 2, // top
+    cameraHeight / -2, // bottom
+    0, // near plane
+    1000 // far plane
+);
+camera.position.set(100, 100, 100);
+camera.lookAt(0, 0, 0);
+camupx.onclick = function (){
+    camera.up.set(1,0,0);
+    camera.lookAt(0, 0, 0);
+}
+
+camupz.onclick = function (){
+    camera.up.set(0,0,1);
+    camera.lookAt(0, 0, 0);
+}
+
+camupy.onclick = function (){
+    camera.up.set(0,1,);
+    camera.lookAt(0, 0, 0);
+}
+
+
+
+
+camdist.onchange = function(){
+    var cameraVal = camdist.value
+    console.log("cameraVal", cameraVal)
+    camera = new THREE.OrthographicCamera(-1*cameraVal, 1*cameraVal, 1*cameraVal, -1*cameraVal,
+        0,
+        1000)
+    camera.position.set(100, 100, 100);
+    camera.lookAt(0, 10, 0);
+
+}
+
+
+const camposx = document.getElementById('camposx')
+const camposy = document.getElementById('camposy')
+const camposz = document.getElementById('camposz')
+camposx.onchange = camposy.onchange = camposz   .onchange =function(){
+    var camposvalx = camposx.value;
+    var camposvaly = camposy.value;
+    var camposvalz = camposz.value;
+    camera.position.set(camposvalx, camposvaly, camposvalz);
+    camera.lookAt(0, 10, 0);
+
+}
+
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+var axesHelper = new THREE.AxesHelper( 55 );
+scene.add( axesHelper );
+
+function createWheels() {
+    const geometry = new THREE.BoxBufferGeometry(12, 12, 33);
+    const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+    const wheel = new THREE.Mesh(geometry, material);
+    return wheel;
+}
+
+
+function createCar() {
+    const car = new THREE.Group();
+
+    const backWheel = createWheels();
+    backWheel.position.y = 6;
+    backWheel.position.x = -18;
+    car.add(backWheel);
+
+    const frontWheel = createWheels();
+    frontWheel.position.y = 6;
+    frontWheel.position.x = 18;
+    car.add(frontWheel);
+
+    const main = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(60, 15, 30),
+        new THREE.MeshLambertMaterial({ color: 0x78b14b })
+    );
+    main.position.y = 12;
+    car.add(main);
+
+    const cabin = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(33, 12, 24),
+        new THREE.MeshLambertMaterial({ color: 0xffffff })
+    );
+    cabin.position.x = -6;
+    cabin.position.y = 25.5;
+    car.add(cabin);
+
+    return car;
+}
+
+const car = createCar();
+scene.add(car);
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+animate();
---
+++ examples/earth/main.js
@@ -0,0 +1,80 @@
+import * as THREE from '../../node_modules/three/src/Three.js';
+
+function main(){
+    const canvas = document.querySelector("#c");
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    renderer.setClearColor(0x000000, 1.0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const fov = 40;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 50, 0);
+    camera.up.set(0, 0, 1);
+    camera.lookAt(0, 0, 0);
+
+
+    const scene = new THREE.Scene();
+
+    const axesHelper = new THREE.AxesHelper(55);
+    scene.add(axesHelper);
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 3;
+        const light = new THREE.PointLight(color, intensity);
+        scene.add(light);
+    }
+
+
+    // an array of objects who's rotation to update
+    const objects = [];
+
+    const radius = 1;
+    const widthSegments = 6;
+    const heightSegments = 6;
+    const sphereGeometry = new THREE.SphereGeometry(
+        radius, widthSegments, heightSegments);
+
+    const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+    sunMesh.scale.set(5, 5, 5);
+    scene.add(sunMesh);
+    objects.push(sunMesh);
+
+    // function resizeRendererToDisplaySize(renderer) {
+    //     const canvas = renderer.domElement;
+    //     const width = canvas.clientWidth;
+    //     const height = canvas.clientHeight;
+    //     const needResize = canvas.width !== width || canvas.height !== height;
+    //     if (needResize) {
+    //         renderer.setSize(width, height, false);
+    //     }
+    //     return needResize;
+    // }
+
+    function render(time) {
+        time *= 0.001;
+
+        // if (resizeRendererToDisplaySize(renderer)) {
+        //     const canvas = renderer.domElement;
+        //     camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        //     camera.updateProjectionMatrix();
+        // }
+
+        objects.forEach((obj) => {
+            obj.rotation.y = time;
+        });
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+}
+
+main();
